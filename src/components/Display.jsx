ef07eb653d9aa3d910f9d5491db54499c0afaa38
reshad21 <miakala7@gmail.com>
2023-05-15 16:58:45 +0600

    farmar animation add

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,4 +1,5 @@
 // eslint-disable-next-line no-unused-vars
+import { motion } from "framer-motion";
 import React from 'react';
 import { useProducts } from '../context/ProductProvider';
 import './Display.css';
@@ -10,45 +11,115 @@ const Display = () => {
     <div className={`w-[80%] h-[80%]  mx-auto ${showComponent ? 'display cs-glass' : 'hide cs-glass'}`}>
       <div className={`grid grid-cols-7 gap-y-5 p-4 content-center `}>
         {
-          // showComponent &&
+          showComponent &&
           <>
-            <div className={`p-4 w-28 h-28 rounded-md bg-[#FFFAEF] mx-auto  ${showComponent ? 'itemShow' : 'itemHide'} `}>
-              <img className='hover:scale-110 duration-300 cursor-pointer' src="https://img.icons8.com/?size=512&id=103424&format=png" alt="" />
-            </div>
-            <div className={`p-4 w-28 h-28 rounded-md bg-[#FFFAEF] mx-auto  ${showComponent ? 'itemShow' : 'itemHide'} `}>
-              <img className='hover:scale-110 duration-300 cursor-pointer' src="https://img.icons8.com/?size=512&id=74268&format=png" alt="" />
-            </div>
-            <div className={`p-4 w-28 h-28 rounded-md bg-[#FFFAEF] mx-auto  ${showComponent ? 'itemShow' : 'itemHide'} `}>
-              <img className='hover:scale-110 duration-300 cursor-pointer' src="https://img.icons8.com/?size=512&id=103424&format=png" alt="" />
-            </div>
-            <div className={`p-4 w-28 h-28 rounded-md bg-[#FFFAEF] mx-auto  ${showComponent ? 'itemShow' : 'itemHide'} `}>
-              <img className='hover:scale-110 duration-300 cursor-pointer' src="https://img.icons8.com/?size=512&id=104996&format=png" alt="" />
-            </div>
-            <div className={`p-4 w-28 h-28 rounded-md bg-[#FFFAEF] mx-auto  ${showComponent ? 'itemShow' : 'itemHide'} `}>
-              <img className='hover:scale-110 duration-300 cursor-pointer' src="https://img.icons8.com/?size=512&id=104996&format=png" alt="" />
-            </div>
-            <div className={`p-4 w-28 h-28 rounded-md bg-[#FFFAEF] mx-auto  ${showComponent ? 'itemShow' : 'itemHide'} `}>
-              <img className='hover:scale-110 duration-300 cursor-pointer' src="https://img.icons8.com/?size=512&id=eHKMKjewguCH&format=png" alt="" />
-            </div>
-            <div className={`p-4 w-28 h-28 rounded-md bg-[#FFFAEF] mx-auto  ${showComponent ? 'itemShow' : 'itemHide'} `}>
-              <img className='hover:scale-110 duration-300 cursor-pointer' src="https://img.icons8.com/?size=512&id=101670&format=png" alt="" />
-            </div>
+            <motion.div
+              initial={{ opacity: 0, scale: 0, x: '-100vw', y: '100vw' }}
+              animate={{ opacity: 1, scale: 1, x: 0, y: 0 }}
+              transition={{ duration: 0.2 }}
 
-            <div className={`p-4 w-28 h-28 rounded-md bg-[#FFFAEF] mx-auto  ${showComponent ? 'sevenItemShow' : 'sevenItemHide'} `}>
+              className={`p-4 w-28 h-28 rounded-md bg-[#FFFAEF] mx-auto `}>
               <img className='hover:scale-110 duration-300 cursor-pointer' src="https://img.icons8.com/?size=512&id=103424&format=png" alt="" />
-            </div>
-            <div className={`p-4 w-28 h-28 rounded-md bg-[#FFFAEF] mx-auto  ${showComponent ? 'itemShow' : 'itemHide'} `}>
+            </motion.div>
+
+            <motion.div
+              initial={{ opacity: 0, scale: 0, x: '-100vw', y: '100vw' }}
+              animate={{ opacity: 1, scale: 1, x: 0, y: 0 }}
+              transition={{ duration: 0.3 }}
+
+              className={`p-4 w-28 h-28 rounded-md bg-[#FFFAEF] mx-auto `}>
+              <img className='hover:scale-110 duration-300 cursor-pointer' src="https://img.icons8.com/?size=512&id=74268&format=png" alt="" />
+            </motion.div>
+
+            <motion.div
+              initial={{ opacity: 0, scale: 0, x: '-100vw', y: '100vw' }}
+              animate={{ opacity: 1, scale: 1, x: 0, y: 0 }}
+              transition={{ duration: 0.4 }}
+
+              className={`p-4 w-28 h-28 rounded-md bg-[#FFFAEF] mx-auto `}>
+              <img className='hover:scale-110 duration-300 cursor-pointer' src="https://img.icons8.com/?size=512&id=103424&format=png" alt="" />
+            </motion.div>
+
+            <motion.div
+              initial={{ opacity: 0, scale: 0, x: '-100vw', y: '100vw' }}
+              animate={{ opacity: 1, scale: 1, x: 0, y: 0 }}
+              transition={{ duration: 0.5 }}
+
+              className={`p-4 w-28 h-28 rounded-md bg-[#FFFAEF] mx-auto `}>
               <img className='hover:scale-110 duration-300 cursor-pointer' src="https://img.icons8.com/?size=512&id=104996&format=png" alt="" />
-            </div>
-            <div className={`p-4 w-28 h-28 rounded-md bg-[#FFFAEF] mx-auto  ${showComponent ? 'itemShow' : 'itemHide'} `}>
+            </motion.div>
+
+            <motion.div
+              initial={{ opacity: 0, scale: 0, x: '-100vw', y: '100vw' }}
+              animate={{ opacity: 1, scale: 1, x: 0, y: 0 }}
+              transition={{ duration: 0.6 }}
+
+              className={`p-4 w-28 h-28 rounded-md bg-[#FFFAEF] mx-auto `}>
               <img className='hover:scale-110 duration-300 cursor-pointer' src="https://img.icons8.com/?size=512&id=104996&format=png" alt="" />
-            </div>
-            <div className={`p-4 w-28 h-28 rounded-md bg-[#FFFAEF] mx-auto  ${showComponent ? 'itemShow' : 'itemHide'} `}>
+            </motion.div>
+
+            <motion.div
+              initial={{ opacity: 0, scale: 0, x: '-100vw', y: '100vw' }}
+              animate={{ opacity: 1, scale: 1, x: 0, y: 0 }}
+              transition={{ duration: 0.7 }}
+
+              className={`p-4 w-28 h-28 rounded-md bg-[#FFFAEF] mx-auto `}>
               <img className='hover:scale-110 duration-300 cursor-pointer' src="https://img.icons8.com/?size=512&id=eHKMKjewguCH&format=png" alt="" />
-            </div>
-            <div className={`p-4 w-28 h-28 rounded-md bg-[#FFFAEF] mx-auto  ${showComponent ? 'itemShow' : 'itemHide'} `}>
+            </motion.div>
+
+            <motion.div
+              initial={{ opacity: 0, scale: 0, x: '-100vw', y: '100vw' }}
+              animate={{ opacity: 1, scale: 1, x: 0, y: 0 }}
+              transition={{ duration: 0.8 }}
+
+              className={`p-4 w-28 h-28 rounded-md bg-[#FFFAEF] mx-auto `}>
               <img className='hover:scale-110 duration-300 cursor-pointer' src="https://img.icons8.com/?size=512&id=101670&format=png" alt="" />
-            </div>
+            </motion.div>
+
+            <motion.div
+              initial={{ opacity: 0, scale: 0, x: '-100vw', y: '100vw' }}
+              animate={{ opacity: 1, scale: 1, x: 0, y: 0 }}
+              transition={{ duration: 0.9 }}
+
+              className={`p-4 w-28 h-28 rounded-md bg-[#FFFAEF] mx-auto  `}>
+              <img className='hover:scale-110 duration-300 cursor-pointer' src="https://img.icons8.com/?size=512&id=103424&format=png" alt="" />
+            </motion.div>
+
+            <motion.div
+              initial={{ opacity: 0, scale: 0, x: '-100vw', y: '100vw' }}
+              animate={{ opacity: 1, scale: 1, x: 0, y: 0 }}
+              transition={{ duration: 1 }}
+
+              className={`p-4 w-28 h-28 rounded-md bg-[#FFFAEF] mx-auto `}>
+              <img className='hover:scale-110 duration-300 cursor-pointer' src="https://img.icons8.com/?size=512&id=104996&format=png" alt="" />
+            </motion.div>
+
+            <motion.div
+              initial={{ opacity: 0, scale: 0, x: '-100vw', y: '100vw' }}
+              animate={{ opacity: 1, scale: 1, x: 0, y: 0 }}
+              transition={{ duration: 1.1 }}
+
+              className={`p-4 w-28 h-28 rounded-md bg-[#FFFAEF] mx-auto `}>
+              <img className='hover:scale-110 duration-300 cursor-pointer' src="https://img.icons8.com/?size=512&id=104996&format=png" alt="" />
+            </motion.div>
+
+            <motion.div
+              initial={{ opacity: 0, scale: 0, x: '-100vw', y: '100vw' }}
+              animate={{ opacity: 1, scale: 1, x: 0, y: 0 }}
+              transition={{ duration: 1.2 }}
+
+              className={`p-4 w-28 h-28 rounded-md bg-[#FFFAEF] mx-auto `}>
+              <img className='hover:scale-110 duration-300 cursor-pointer' src="https://img.icons8.com/?size=512&id=eHKMKjewguCH&format=png" alt="" />
+            </motion.div>
+
+            <motion.div
+              initial={{ opacity: 0, scale: 0, x: '-100vw', y: '100vw' }}
+              animate={{ opacity: 1, scale: 1, x: 0, y: 0 }}
+              transition={{ duration: 1.3 }}
+
+              className={`p-4 w-28 h-28 rounded-md bg-[#FFFAEF] mx-auto `}>
+              <img className='hover:scale-110 duration-300 cursor-pointer' src="https://img.icons8.com/?size=512&id=101670&format=png" alt="" />
+            </motion.div>
           </>
         }
       </div>
